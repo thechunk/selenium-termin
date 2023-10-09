a97e580d30ff2e605802f0a20ae693e770fbcad7
Russell <russellc@Russells-MacBook-Pro.local>
2023-10-09 22:34:59 +0200

    fix extra logger in init

--- lib/data/connection.rb
+++ lib/data/connection.rb
@@ -5,7 +5,7 @@ module Termin
 
       def initialize(logger:, path:)
         @logger = logger
-        @schema = Sequel.sqlite(path, loggers: [@logger])
+        @schema = Sequel.sqlite(path)
         @schema.loggers << logger if ENV['RUBY_ENV'] == 'development'
       end
 
