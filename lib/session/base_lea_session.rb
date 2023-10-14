44950a3b534107d3bb4ace0869641190267064eb
Russell <russell@replo.de>
2023-10-14 11:25:17 +0200

    extract base lea session logic

---
+++ lib/session/base_lea_session.rb
@@ -0,0 +1,59 @@
+require_relative 'base_session.rb'
+
+module Termin
+  module Session
+    class BaseLeaSession < BaseSession
+      def initialize(logger: nil, notifier: nil, driver: nil)
+        super(logger:, driver:)
+
+        @notifier = notifier
+        @logger = logger
+      end
+
+      def root_url
+        'https://otv.verwalt-berlin.de/ams/TerminBuchen?lang=en&termin=1&dienstleister=327437&anliegen[]=328188'
+      end
+
+      def form
+        raise NotImplementedError
+      end
+
+      def call
+        loading_wait
+        click(css: '.slide-content .link > a')
+
+        loading_wait
+        click(name: 'gelesen')
+
+        loading_wait
+        click(id: 'applicationForm:managedForm:proceed')
+
+        loading_wait
+        form.populate
+
+        loading_wait
+        click(id: 'applicationForm:managedForm:proceed')
+
+        loading_wait
+        begin
+          messages_box = wait_for_element(id: 'messagesBox') do |element|
+            element.displayed? && element.text.length > 0
+          end
+          no_dates_error = 'There are currently no dates available for the selected service! Please try again later.'
+          no_dates = messages_box.text == no_dates_error
+
+          raise RunFailError.new(no_dates_error) if no_dates
+
+          wait_for_element(id: 'xi-fs-2')
+
+          @notifier.broadcast(text: 'Appointments available')
+        rescue Selenium::WebDriver::Error::TimeoutError => e
+          @logger.info("messages_box: #{messages_box}")
+          @logger.info("no_dates: #{no_dates}")
+
+          raise
+        end
+      end
+    end
+  end
+end
--- lib/session/lea_extend.rb
+++ lib/session/lea_extend.rb
@@ -1,29 +1,8 @@
 module Termin
   module Session
-    class LeaExtend < BaseSession
-      def initialize(logger: nil, notifier: nil, driver: nil)
-        super(logger:, driver:)
-
-        @notifier = notifier
-        @logger = logger
-      end
-
-      def root_url
-        'https://otv.verwalt-berlin.de/ams/TerminBuchen?lang=en&termin=1&dienstleister=327437&anliegen[]=328188'
-      end
-
-      def call
-        loading_wait
-        click(css: '.slide-content .link > a')
-
-        loading_wait
-        click(name: 'gelesen')
-
-        loading_wait
-        click(id: 'applicationForm:managedForm:proceed')
-
-        loading_wait
-        form = Form.new(self, [
+    class LeaExtend < BaseLeaSession
+      def form
+        Form.new(self, [
           { name: 'sel_staat', value: 'China' },
           { name: 'personenAnzahl_normal', value: 'one person' },
           { name: 'lebnBrMitFmly', value: 'yes' },
@@ -31,30 +10,7 @@ module Termin
           { css: '[for="SERVICEWAHL_EN3479-0-2"]' },
           { css: '[for="SERVICEWAHL_EN_479-0-2-4"]' },
           { css: '[for="SERVICEWAHL_EN479-0-2-4-305289"]' }
-        ]).populate
-
-        loading_wait
-        click(id: 'applicationForm:managedForm:proceed')
-
-        loading_wait
-        begin
-          messages_box = wait_for_element(id: 'messagesBox') do |element|
-            element.displayed? && element.text.length > 0
-          end
-          no_dates_error = 'There are currently no dates available for the selected service! Please try again later.'
-          no_dates = messages_box.text == no_dates_error
-
-          raise RunFailError.new(no_dates_error) if no_dates
-
-          wait_for_element(id: 'xi-fs-2')
-
-          @notifier.broadcast(text: 'Appointments available')
-        rescue Selenium::WebDriver::Error::TimeoutError => e
-          @logger.info("messages_box: #{messages_box}")
-          @logger.info("no_dates: #{no_dates}")
-
-          raise
-        end
+        ])
       end
     end
   end
